800c0007d04c3dca55432075d0f33f307d64e806
{ElenaKovaleva} <{lenshemeta@gmail.com}>
2024-06-17 10:18:04 +0300

    added homework

---
+++ hw01-xml-config/src/main/java/ru/otus/spring/dao/CsvQuestionDao.java
@@ -0,0 +1,23 @@
+package ru.otus.spring.dao;
+
+import lombok.RequiredArgsConstructor;
+import ru.otus.spring.config.TestFileNameProvider;
+import ru.otus.spring.domain.Question;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CsvQuestionDao implements QuestionDao {
+    private final TestFileNameProvider fileNameProvider;
+
+    @Override
+    public List<Question> findAll() {
+        // Использовать CsvToBean
+        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
+        // Использовать QuestionReadException
+        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+
+        return new ArrayList<>();
+    }
+}
