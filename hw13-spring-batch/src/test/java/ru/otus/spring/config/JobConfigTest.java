0c6de5641f1ade18c73024d5ec78142cc89074b2
ElenaKovaleva <lenshemeta@gmail.com>
2024-12-04 16:32:27 +0300

    hw13 fixed tests

--- hw13-spring-batch/src/test/java/ru/otus/spring/config/JobConfigTest.java
+++ hw13-spring-batch/src/test/java/ru/otus/spring/config/JobConfigTest.java
@@ -1,6 +1,5 @@
 package ru.otus.spring.config;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.batch.core.Job;
