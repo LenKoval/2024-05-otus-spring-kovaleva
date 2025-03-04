4710ec79e04b5dcafcdf5ef9b08023c2672121fc
ElenaKovaleva <lenshemeta@gmail.com>
2025-03-04 14:03:18 +0300

    hw15 done

--- hw15-spring-actuator/src/main/java/ru/otus/spring/repositories/AuthorRepository.java
+++ hw15-spring-actuator/src/main/java/ru/otus/spring/repositories/AuthorRepository.java
@@ -1,7 +1,9 @@
 package ru.otus.spring.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.otus.spring.models.Author;
 
+@RepositoryRestResource(path = "authors")
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 }
--- hw15-spring-actuator/src/main/java/ru/otus/spring/repositories/CommentRepository.java
+++ hw15-spring-actuator/src/main/java/ru/otus/spring/repositories/CommentRepository.java
@@ -4,14 +4,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import ru.otus.spring.models.Comment;
 
 import java.util.List;
 
+@RepositoryRestResource(path = "comments")
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    @RestResource(path = "by-bookId", rel = "comments-by-bookId")
     List<Comment> findCommentsByBookId(long bookId);
 
+    @RestResource(path = "delete-by-bookId", rel = "delete-comments-by-bookId")
     @Modifying(flushAutomatically = true, clearAutomatically = true)
     @Query("delete from Comment c where c.book.id = :bookId")
     void deleteByBookId(@Param("bookId") Long bookId);
